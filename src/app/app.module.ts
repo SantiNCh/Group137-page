8485b5ad3ddf04cbdf39ea74b7bed2fc6ee16a35
Exxxosis <santiagochantada@gmail.com>
2020-02-11 03:47:54 -0300

    Calendar, photos and contact component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PhotosComponent,
+    ContactComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule

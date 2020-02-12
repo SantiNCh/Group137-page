9e00ea65039e158b65cffb6a28965992ca0c1a9a
Exxxosis <santiagochantada@gmail.com>
2020-02-11 22:44:24 -0300

    Add updateProject method

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -57,7 +57,18 @@ var controller = {
 		});
 	},
 
-	
+	updateProject: function(req, res){
+		var projectId = req.params.id;
+		var update = req.body;
+
+		Project.findByIdAndUpdate(projectId, update, {new: true}, (err, projectUpdated) => {
+			if(err) return res.status(500).send({message:"Error al actualizar los datos"});
+
+			if (!projectUpdated) return res.status(404).send({message:"No hay proyectos para actualizar"});
+
+			return res.status(200).send({project: projectUpdated}); 
+		});
+	},
 
 };
 

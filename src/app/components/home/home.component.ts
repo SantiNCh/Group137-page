78924b77c59da1ec01930cd6b1f8e55e755494e1
Exxxosis <santiagochantada@gmail.com>
2020-02-13 14:12:29 -0300

    Add momentjs, luxon packages and edit the home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 declare var $:any;
+import * as moment from 'moment';
+import 'moment/locale/es';
+
 
 @Component({
   selector: 'app-home',
@@ -26,8 +29,46 @@ export class HomeComponent implements OnInit {
           slideWidth: 1200
     });
 
+    //Posts
 
+    var posts = [
+        {
+          title: 'Prueba de titulo 1',
+          date: 'Publicado el dia '+ moment().day() + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY') ,
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla dignissim odio, vehicula tincidunt nisl mattis et. Vivamus consequat justo sit amet viverra auctor. Ut sed accumsan metus. Fusce id arcu at enim pellentesque varius. Cras lorem magna, accumsan a cursus nec, eleifend nec ex. Phasellus enim purus, viverra sed suscipit scelerisque, lobortis a magna. Morbi venenatis tellus id ex euismod laoreet. Sed vitae pretium libero. Proin et quam augue. Nunc eget convallis metus. Curabitur varius quis massa at fringilla. Ut vehicula risus odio, sed elementum nisi maximus at. In sollicitudin consequat dolor sed auctor. Praesent consectetur rutrum nisi, ut malesuada urna consequat quis.'
+        },
+        {
+          title: 'Prueba de titulo 2',
+          date: 'Publicado el dia '+ moment().day() + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY') ,
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla dignissim odio, vehicula tincidunt nisl mattis et. Vivamus consequat justo sit amet viverra auctor. Ut sed accumsan metus. Fusce id arcu at enim pellentesque varius. Cras lorem magna, accumsan a cursus nec, eleifend nec ex. Phasellus enim purus, viverra sed suscipit scelerisque, lobortis a magna. Morbi venenatis tellus id ex euismod laoreet. Sed vitae pretium libero. Proin et quam augue. Nunc eget convallis metus. Curabitur varius quis massa at fringilla. Ut vehicula risus odio, sed elementum nisi maximus at. In sollicitudin consequat dolor sed auctor. Praesent consectetur rutrum nisi, ut malesuada urna consequat quis.'
+        },
+        {
+          title: 'Prueba de titulo 3',
+          date: 'Publicado el dia '+ moment().day() + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY') ,          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla dignissim odio, vehicula tincidunt nisl mattis et. Vivamus consequat justo sit amet viverra auctor. Ut sed accumsan metus. Fusce id arcu at enim pellentesque varius. Cras lorem magna, accumsan a cursus nec, eleifend nec ex. Phasellus enim purus, viverra sed suscipit scelerisque, lobortis a magna. Morbi venenatis tellus id ex euismod laoreet. Sed vitae pretium libero. Proin et quam augue. Nunc eget convallis metus. Curabitur varius quis massa at fringilla. Ut vehicula risus odio, sed elementum nisi maximus at. In sollicitudin consequat dolor sed auctor. Praesent consectetur rutrum nisi, ut malesuada urna consequat quis.'
+        },
+        {
+          title: 'Prueba de titulo 4',
+          date: 'Publicado el dia '+ moment().day() + ' de ' + moment().format('MMMM') + ' del ' + moment().format('YYYY') ,
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla dignissim odio, vehicula tincidunt nisl mattis et. Vivamus consequat justo sit amet viverra auctor. Ut sed accumsan metus. Fusce id arcu at enim pellentesque varius. Cras lorem magna, accumsan a cursus nec, eleifend nec ex. Phasellus enim purus, viverra sed suscipit scelerisque, lobortis a magna. Morbi venenatis tellus id ex euismod laoreet. Sed vitae pretium libero. Proin et quam augue. Nunc eget convallis metus. Curabitur varius quis massa at fringilla. Ut vehicula risus odio, sed elementum nisi maximus at. In sollicitudin consequat dolor sed auctor. Praesent consectetur rutrum nisi, ut malesuada urna consequat quis.'
+        }
 
+    ];
+    
+   posts.forEach((item, index) => {
+      var about = `
+        <article class="about">
+          <h2>${item.title}</h2>
+          <span class="date">${item.date}</span>
+          <p>
+            ${item.content}  
+          </p>
+          <a href="#" class="button-more">Leer mas</a>
+        </article>
+
+      `;
+
+      $("#posts").append(about);
+    });
 
   });
 
